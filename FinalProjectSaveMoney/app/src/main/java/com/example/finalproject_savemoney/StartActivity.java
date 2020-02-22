7adb4be4aca3f39eea904ebb2e1de08679118375
Eugene Poroshin <zhenkanet@gmail.com>
2020-02-22 22:19:24 +0300

    first commit

---
+++ FinalProjectSaveMoney/app/src/main/java/com/example/finalproject_savemoney/StartActivity.java
@@ -0,0 +1,24 @@
+package com.example.finalproject_savemoney;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        findViewById(R.id.buttonStart).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
